841730e803e94b4e7876abc7b712f5c840d32d46
Мішаня <ros.mykhailo@lll.kpi.ua>
2025-08-08 15:19:53 +0300

    feat: add (LC9)

---
+++ src/alghoritms/palindromenumber.java
@@ -0,0 +1,20 @@
+package alghoritms;
+
+public class palindromenumber
+{
+    public boolean isPalindrome(int x)
+    {
+        if(x < 0) return false;
+
+        int copyOfX = x;
+        int res = 0;
+
+        while(copyOfX >= 1)
+        {
+            res = res * 10 + copyOfX % 10;
+            copyOfX /= 10;
+        }
+
+        return res == x;
+    }
+}
